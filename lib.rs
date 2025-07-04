0dd9162a855795073b26a35a702ba142753523a3
joaco <joaquinespeche491@gmail.com>
2025-07-03 22:18:06 -0300

    Publicacion de productos

--- lib.rs
+++ lib.rs
@@ -2,11 +2,11 @@
 
 #[ink::contract]
 mod marketplace {
-    
+    use ink::prelude::{string::String, vec::Vec};
     use ink::storage::Mapping;
 
 
-    #[derive(Clone)]
+    #[derive(Clone,PartialEq, Eq)]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     #[cfg_attr(
         feature = "std",
@@ -28,17 +28,35 @@ mod marketplace {
         YaRegistrado,
         UsuarioNoRegistrado,
     }
+        
+    #[derive(Clone)]
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    #[cfg_attr(
+        feature = "std",
+        derive(ink::storage::traits::StorageLayout)
+    )]
+    pub struct Producto{
+        pub nombre:String,
+        pub descripcion:String,
+        pub precio: u128,
+        pub cantidad: u32,
+        pub categoria:String,
+    }
+
 
     #[ink(storage)]
     pub struct RegistroUsuarios {
         roles: Mapping<AccountId, Roles>,
+        productos: Mapping<AccountId, Vec<Producto>>,
     }
 
     impl RegistroUsuarios {
+        // SISTEMA DE GESTION DE USUARIOS
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {
                 roles: Mapping::default(),
+                productos: Mapping::default(), 
             }
         }
 
@@ -66,7 +84,47 @@ mod marketplace {
         pub fn obtener_rol(&self, usuario: AccountId) -> Option<Roles> {
             self.roles.get(usuario)
         }
+        // PUBLICACION DE PRODUCTOS
+        #[ink(message)]
+        pub fn publicar_producto(
+            &mut self,
+            nombre: String,
+            descripcion: String,
+            precio: u128,
+            cantidad: u32,
+            categoria: String,
+        ) {
+            let caller = self.env().caller();
+
+            // Verificamos si el usuario tiene rol de Vendedor
+            let rol = self.roles.get(&caller);
+            assert!(
+                matches!(rol, Some(Roles::Vendedor) | Some(Roles::Ambos)),
+                "No sos vendedor"
+            );
+            let producto = Producto {
+                nombre,
+                descripcion,
+                precio,
+                cantidad,
+                categoria,
+            };
+
+            let mut lista = self.productos.get(&caller).unwrap_or_default();
+            lista.push(producto);
+            self.productos.insert(&caller, &lista);
+        }
+
+
+        #[ink(message)]
+        pub fn ver_mis_productos(&self) -> Vec<Producto> {
+            let caller = self.env().caller();
+            self.productos.get(&caller).unwrap_or_default()
+        }
+
+        
     }
+
 
 
 }
